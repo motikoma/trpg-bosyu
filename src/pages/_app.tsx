5d3e3b2c62d68d8f773839814fe55fb25a243749
motikoma <motikoma5125@gmail.com>
2020-11-28 23:10:16 +0900

    fix: SSRに関する修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,25 @@
-
-import React,{useEffect} from "react";
-import '../../styles/globals.css';
-import { ThemeProvider } from '@material-ui/core';
-import CssBaseline from '@material-ui/core/CssBaseline'
-import { AppProps } from 'next/app';
+import React, { useEffect } from "react";
+import "../../styles/globals.css";
+import { ThemeProvider } from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { AppProps } from "next/app";
 import theme from "../../styles/theme";
 
-const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   // Remove the server-side injected CSS.(https://material-ui.com/guides/server-rendering/)
   useEffect(() => {
-    const jssStyles = document.querySelector('#jss-server-side')
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
-      jssStyles.parentNode.removeChild(jssStyles)
+      jssStyles.parentElement.removeChild(jssStyles);
     }
-  }, [])
+  }, []);
 
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
-}
+};
 
-export default MyApp
+export default App;
